2af9d41e4ee31d658a890423cd2633461a5b878c
menglim <smaemenglim@gmail.com>
2021-09-19 16:09:25 +0700

    * Add Smb

--- src/main/java/com/github/menglim/mutils/AppUtils.java
+++ src/main/java/com/github/menglim/mutils/AppUtils.java
@@ -47,6 +47,8 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.activation.MimetypesFileTypeMap;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.net.ssl.HttpsURLConnection;
@@ -70,6 +72,7 @@ import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.Key;
 import java.security.SecureRandom;
 import java.text.DecimalFormat;
 import java.text.ParseException;
@@ -1771,6 +1774,36 @@ public class AppUtils {
         }
         //System.out.println(fList);
         return resultList;
+    }
+
+    public String encrypt(String plainText, String keyPhrase) throws Exception {
+        if(keyPhrase.length()!=16){
+            throw new  Exception("Key must be in 16 digits");
+        }
+        try {
+            Key aesKey = new SecretKeySpec(keyPhrase.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+            byte[] encrypted = cipher.doFinal(plainText.getBytes());
+            return AppUtils.getInstance().toBase64(encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String decrypt(String encryptedBase64Text, String keyPhrase) {
+        try {
+            Key aesKey = new SecretKeySpec(keyPhrase.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, aesKey);
+            byte[] encryptedText = AppUtils.getInstance().fromBase64ToByte(encryptedBase64Text);
+            String decrypted = new String(cipher.doFinal(encryptedText));
+            return decrypted;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
